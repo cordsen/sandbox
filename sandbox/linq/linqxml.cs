f215c3e49d1ed461dd6cffa141de79c72e5829c4
erik <cordsen@gmail.com>
2011-06-30 22:35:42 -0400

    initial commit

---
+++ sandbox/linq/linqxml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace sandbox.linq
+{
+    class linqxml
+    {
+        public static XElement XElem()
+        {
+            XElement xml = new XElement("contacts",
+                                new XElement("contact",
+                                    new XAttribute("contactId", "2"),
+                                    new XElement("firstName", "foo"),
+                                    new XElement("lastName", "bar")
+                                ),
+                                new XElement("contact",
+                                    new XAttribute("contactId", "3"),
+                                    new XElement("firstName", "erik"),
+                                    new XElement("lastName", "cord")
+                                )
+                            );
+            return xml;
+        }
+
+        public static 
+    }
+}
