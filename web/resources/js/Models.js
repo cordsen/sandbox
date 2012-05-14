2aa0d331343bf27e2b35c21bd7f4fd64e0627996
Erik Cordsen <cordsen@gmail.com>
2012-05-13 23:17:37 -0400

    working on models and views

--- web/resources/js/Models.js
+++ web/resources/js/Models.js
@@ -1,5 +1,5 @@
-﻿// Image Model - used for a generic image container in the select view
-(function($) {
+﻿(function($) {
+// Image Model - used for a generic image container in the select view
     "use strict";
     var Image = Backbone.Model.extend({
         // Standard attributes
@@ -9,7 +9,7 @@
         tags: [],
         captureDate: "",
         captureLocation: { lat: "", lon: "" },
-        iconUri: "",
+        iconUri: "http://apps.ezprints.com/Assets/Artwork/Utility/SampleSource/Stock/6863944_00000_thumb.jpg",
         screenUri: "",
         printUri: "",
         printSize: "",
@@ -29,37 +29,59 @@
     });
 
     var ImageList = Backbone.Collection.extend({
-        model: Image
-    });
+        model: Image,
 
-    var ImageViewList = Backbone.View.extend({
-        el: 'body',
-        initialize: function() {
-            _.bindAll(this, 'render', 'addItem', 'appendItem'); // every function that uses 'this' as the current object should be in here
-
-            this.collection = new ImageList();
-            this.collection.bind('add', this.appendItem); // collection event binder
-
-            this.counter = 0;
-            this.render();
-        }
     });
 
     var ImageView = Backbone.View.extend({
-        el: '#ezp_image_list',
+        el: 'div',
 
         initialize: function() {
             _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
-            this.render(); // not all views are self-rendering. This one is.
         },
 
-        render: function() {
-            var newImage = $('#ezp_image').clone();
-            newImage.find("img.image").attr('src', 'http://apps.ezprints.com/Assets/Artwork/Utility/SampleSource/Stock/6863944_00000_thumb.jpg');
-            $(this.el).append(newImage);
+        render: function(list) {
+            this.el = $('#ezp_image').clone();
+			this.el.attr('class', '');
+            this.el.find("img.image").attr('src', this.model.iconUri);
+            return this.el;
         }
 
     });
 
-    var photo = new ImageView();
+    var ImageViewList = Backbone.View.extend({
+        el : 'div',
+
+		events : {
+			'click button#addPhoto:' : 'addItem'
+		},
+		
+		initialize: function() {
+            _.bindAll(this, 'render', 'addItem'); // every function that uses 'this' as the current object should be in here
+
+            this.collection = new ImageList();
+            //this.collection.bind('addPhoto', this.appendItem); // collection event binder
+
+            this.render();
+        },
+		
+		render : function () {
+			this.el = $('#ezp_image_list_template').clone();
+			this.el.attr('id','photo list');
+			this.el.attr('class', '');
+			$('#ezp_container').append(this.el);
+
+		},
+		
+		addItem : function () {
+			var image = new Image();
+			var iv = new ImageView({model: image});
+			this.collection.add(image);
+			this.el.append(iv.render());
+
+		}
+    });
+
+    //var photo = new ImageView();
+	var list = new ImageViewList();
 })(jQuery);
