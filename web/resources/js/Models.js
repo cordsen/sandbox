bb1496e882ec9ffcd08d2c80ccb293ceab32c2bf
Erik Cordsen <cordsen@gmail.com>
2012-05-22 15:12:24 -0400

    updated css and js

--- web/resources/js/Models.js
+++ web/resources/js/Models.js
@@ -3,37 +3,41 @@
     "use strict";
 
     var root = this;
-    //var ezpimg;
+    var ezpimg;
 
-    var Image = Backbone.Model.extend({
+    var Image = root.Image = Backbone.Model.extend({
         // Standard attributes
-        id: "",
-        title: "",
-        description: "",
-        tags: [],
-        captureDate: "",
-        captureLocation: { lat: "", lon: "" },
-        iconUri: "http://apps.ezprints.com/Assets/Artwork/Utility/SampleSource/Stock/6863944_00000_thumb.jpg",
-        screenUri: "",
-        printUri: "",
-        printSize: "",
-        printWidth: "",
-        printHeight: "",
+        defaults : {
+            /*id: "",
+            title: "",
+            description: "",
+            tags: [],
+            captureDate: "",
+            captureLocation: { lat: "", lon: "" }, */
+            "iconUri": "http://apps.ezprints.com/Assets/Artwork/Utility/SampleSource/Stock/6863944_00000_thumb.jpg"
+            /* screenUri: "",
+            printUri: "",
+            printSize: "",
+            printWidth: "",
+            printHeight: "" */
+        },
 
         // Functions & Overrides
-        initialize: function() { },
+        //initialize: function() { },
 
-        validate: function() {
+        /*validate: function() {
             // make sure screen uri is valid
             if (this.screenUri === "") {
                 return "need at least a screenUri";
             }
-        }
+        }*/
 
     });
 
     var ImageList = Backbone.Collection.extend({
         model: Image,
+
+        
 
     });
 
@@ -46,8 +50,8 @@
 
         render: function(list) {
             this.el = $('#ezp_image').clone();
-			this.el.attr('class', '');
-            this.el.find("img.image").attr('src', this.model.iconUri);
+			this.el.attr('class', 'image');
+            this.el.find("img.image").attr('src', this.model.get('iconUri'));
             return this.el;
         }
 
@@ -57,14 +61,14 @@
         el : 'div',
 
 		events : {
-			'click button#addPhoto:' : 'addItem'
+			'click button#addPhoto:' : 'appendItem'
 		},
 		
 		initialize: function() {
             _.bindAll(this, 'render', 'addItem'); // every function that uses 'this' as the current object should be in here
 
             this.collection = new ImageList();
-            //this.collection.bind('addPhoto', this.appendItem); // collection event binder
+            this.collection.bind('add', this.addItem); // collection event binder
 
             this.render();
         },
@@ -72,20 +76,89 @@
 		render : function () {
 			this.el = $('#ezp_image_list_template').clone();
 			this.el.attr('id','photo list');
-			this.el.attr('class', '');
+			this.el.attr('class', 'span4');
 			$('#ezp_container').append(this.el);
 
 		},
 		
-		addItem : function () {
-			var image = new Image();
+		appendItem : function () {
+			var image = new Image({});
 			var iv = new ImageView({model: image});
 			this.collection.add(image);
-			this.el.append(iv.render());
+			this.el.find('#container').append(iv.render());
+		},
 
+		addItem : function (item) {
+			var iv = new ImageView({model: item});
+			this.el.find('#container').append(iv.render());
 		}
     });
 
     //var photo = new ImageView();
-    root.ezpimg = new ImageViewList();
+    ezpimg = root.ezpimg = new ImageViewList();
+    root.ezp = ezpimg.collection;
+
 }).call(this, jQuery);
+
+
+//ezp.add(new Image());
+
+/* todo
+
+API
+ - add a photo
+ - load a list of photos
+ - load a list of albums
+ - load a collection
+ - show a different collection
+
+View properties
+ - outer container for photos tab: 
+    - includes list of sources and collections(albums) in a mega-dropdown 
+    - includes a list of photos for the current album (photo is a model)
+ - 
+
+View actions
+ - zoom in on a photo
+ - drag/drop a photo on product
+
+
+ Models (JSON)
+  - var source = {
+   name:"facebook",
+   icon:"",
+   albums:[
+      {
+         name:"my friends",
+         icon:"http://some url",
+         assets:[
+            {
+               name:"my photo",
+               imageUrl:"http://bla"
+            },
+            {
+               name:"my photo",
+               imageUrl:"http://bla"
+            }
+         ]
+      },
+      {
+         name:"my other friends",
+         icon:"http://some url",
+         assets:[
+            {
+               name:"my photo",
+               imageUrl:"http://bla"
+            },
+            {
+               name:"my photo",
+               imageUrl:"http://bla"
+            }
+         ]
+      }
+   ]
+};
+*/
+
+
+
