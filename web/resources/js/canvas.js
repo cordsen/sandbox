64ba5da6827da06b8ebc1d3fd008427ed2ff1e82
Erik Cordsen <cordsen@gmail.com>
2012-05-24 15:06:15 -0400

    added canvas

---
+++ web/resources/js/canvas.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var canvas1 = new fabric.Canvas('print-area');
+    canvas1.add(new fabric.Rect({ width: 50, height: 50, fill: 'red', top: 100, left: 100 }));
+    canvas1.add(new fabric.Rect({ width: 30, height: 30, fill: 'green', top: 50, left: 50 }));
+
+    function observe(eventName) {
+        canvas1.observe(eventName, function () { });
+    }
+
+    observe('object:modified');
+    observe('object:moving');
+    observe('object:selected');
+
+    observe('before:selection:cleared');
+    observe('selection:cleared');
+    observe('selection:created');
+
+    // observe('after:render');
+    observe('mouse:up');
+    observe('mouse:down');
+});
